ff35ce002a026669d77c54559525f616c4c58845
Mohammad <mohammadtohidi.mtn@gmail.com>
2024-11-05 10:09:47 +0330

    role manage

---
+++ Divar/Controllers/RolesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Divar.Controllers
+{
+    public class RolesController : Controller
+
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult List()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+
+    }
+}
